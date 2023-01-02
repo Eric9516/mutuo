9719af648a5a129718962d78482797959fa0fd9f
Eric Cantoni <eric-995-@hotmail.com>
2023-01-02 18:20:55 -0300

    cambio de tarjetas

--- src/Styles/StyledPorqueElegirnos.js
+++ src/Styles/StyledPorqueElegirnos.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import fondo from "../img/fondo2.jpg";
 
 export const StyledAllHome = styled.div`
+    width: 100vw;
+    height: 90vh;
     background-color: rgb(230, 228, 228);
     height: auto;
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
+    gap: 20px;
 
     @media screen and (min-width: 1800px) {
         gap: 35px;
@@ -39,11 +41,7 @@ export const StyledDivChild = styled.div`
     flex-direction: column;
     background-color: #fff;
     border-radius: 15px;
-    transition: all 0.2s;
-
-    & p {
-        transform: translateZ(40px);
-    }
+    transition: all 0.6s;
 
     &:hover {
         transform: scale(1.1);
@@ -67,13 +65,16 @@ export const StyledTittle = styled.h3`
 `;
 
 export const StyledLogoWhatsapp = styled.div`
-    width: 60px;
-    height: 60px;
+    width: 75px;
+    height: 75px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     cursor: pointer;
     background-color: #fff;
     border-radius: 50%;
     position: fixed;
-    bottom: 10;
+    bottom: 15px;
     right: 0;
     margin-right: 20px;
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./card.css";
+
+const Card = () => {
+    return (
+        <div class="card">
+            <div class="front">
+                <h1>A</h1>
+            </div>
+            <div class="back">
+                <h1>B</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
