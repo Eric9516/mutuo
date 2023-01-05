4883004e91981f7ad936734f7317cec6f27d1c63
Eric Cantoni <eric-995-@hotmail.com>
2023-01-05 15:46:44 -0300

    optimizando funciones

--- src/Styles/StyledCotizador.js
+++ src/Styles/StyledCotizador.js
@@ -4,3 +4,10 @@ export const StyledCotAll = styled.div`
     width: 100vw;
     height: 100vh;
 `;
+
+export const StyledButton = styled.button`
+    width: 30px;
+    border-radius: 50%;
+`;
+
+
--- src/components/Cotizador.jsx
+++ src/components/Cotizador.jsx
@@ -1,27 +1,24 @@
 import React, { useState } from "react";
-import { StyledCotAll } from "../Styles/StyledCotizador.js";
+import { StyledCotAll, StyledButton } from "../Styles/StyledCotizador.js";
 
 const Cotizador = () => {
     const [cuotas, setCuotas] = useState(3);
 
     const sumar = () => {
         setCuotas(cuotas + 3);
-        if (cuotas === 12) {
-            setCuotas(12);
-        }
+        cuotas === 12 ? setCuotas(12) : sumar();
     };
 
     const restar = () => {
         setCuotas(cuotas - 3);
-        if (cuotas === 3) {
-            setCuotas(3);
-        }
+        cuotas === 3 ? setCuotas(3) : restar();
     };
+
     return (
         <StyledCotAll id="cotizador">
             <h1>Cotizador</h1>
-            <button onClick={sumar}>Sumar</button>
-            <button onClick={restar}>Restar</button>
+            <StyledButton onClick={sumar}>+</StyledButton>
+            <StyledButton onClick={restar}>-</StyledButton>
             <h4>{cuotas}</h4>
         </StyledCotAll>
     );
